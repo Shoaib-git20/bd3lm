d31968ce3f89f9d78f77bb5b28146293bb360eec
Shoaib-git20 <shoaib4c12@gmail.com>
2025-11-08 17:48:17 -0500

    added asycn_tp

--- scripts/train/train_owt_bd3lm.sh
+++ scripts/train/train_owt_bd3lm.sh
@@ -3,11 +3,10 @@
 #SBATCH -o watch_folder/%x_%j.out     # log file (out & err)
 #SBATCH -e watch_folder/%x_%j.err     # log file (out & err)
 #SBATCH --nodes=1                          # Total number of nodes requested
+#SBATCH -p quad
 #SBATCH --gpus-per-node=1                 # Number of gpus per node
-#SBATCH -t 3:00:00                    # Time limit (hh:mm:ss)
+#SBATCH -t 1:00:00                    # Time limit (hh:mm:ss)
 #SBATCH -A PZS0622
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=mohammad.116@osu.edu
 
 BLOCK_SIZE=4
 PRETRAIN_CKPT='' # to train from scratch, set to null
@@ -37,10 +36,10 @@ nvidia-smi
 cd /fs/ess/PZS0622/shoaib/d3pm/bd3lms
 echo "inside ${PWD}"
 
-mpirun -np 4 python -u main.py loader.global_batch_size=16 loader.eval_global_batch_size=16 loader.batch_size=4 loader.eval_batch_size=4 \
-    model=tiny algo=bd3lm algo.clip_search_widths=[0.5] \
+mpirun -np 1 python -u main.py loader.global_batch_size=16 loader.eval_global_batch_size=16 loader.batch_size=16 loader.eval_batch_size=16 \
+    model=small algo=bd3lm algo.clip_search_widths=[0.5] \
     data=openwebtext-split data.insert_train_special=False data.insert_valid_special=False data.insert_valid_eos=False \
-    model.length=128 block_size=${BLOCK_SIZE} wandb.name=bd3lm-owt-block_size${BLOCK_SIZE} \
+    model.length=1024 block_size=${BLOCK_SIZE} wandb.name=bd3lm-owt-block_size${BLOCK_SIZE} \
     mode=train model.attn_backend=flex \
     training.resample=True trainer.fast_dev_run=False
 
